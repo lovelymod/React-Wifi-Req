de30b1aab31ba2bb3a4fe6456684015623c45232
Gong <lovelymod6544@gmail.com>
2022-11-08 10:20:27 +0700

    new

--- src/App.js
+++ src/App.js
@@ -6,56 +6,80 @@ function App() {
     <div className="App">
       
       <div className="header"><h1>Please fill out a request form</h1></div>
-
+      <div className="container">
       <form action="">
 
-            <label htmlFor="inputFname">Firstname :</label>
-            <input type="text" class="" id="inputFirstname" placeholder="Firstname(English)" required/>
+      <label htmlFor="inputFname" className='form-label'>Firstname :</label>
+      <div className="col-sm-10">
+      <input type="text" class="form-control" id="inputFirstname" placeholder="Firstname(English)" required/>
+      </div>
 
-            <label htmlFor="inputLname">Lastname :</label>
-            <input type="text" class="" id="inputLastname" placeholder="Lastname(English)" required/>
+      <label htmlFor="inputLname" className='form-label'>Lastname :</label>
+      <div className="col-sm-10">
+      <input type="text" class="form-control" id="inputLastname" placeholder="Lastname(English)" required/>
+      </div>
 
-            <label htmlFor="UserType"> User Type :</label>
-            <select name="userType" id="inputUsertype">
-                <option value="staff">Staff</option>
-                <option value="internship">Internship</option>
-                <option value="guest">Guest</option>
-            </select>
+      <label htmlFor="UserType" className='form-label'> User Type :</label>
+      <div className="col-sm-10">
+      <select name="userType" className='form-select' id="inputUsertype">
+          <option value="staff">Staff</option>
+          <option value="internship">Internship</option>
+          <option value="guest">Guest</option>
+      </select>
+      </div>
 
-            <label htmlFor="tel">Tel :</label>
-            <input type="number" class="" id="inputTel" placeholder="055xxxxxxx" required/>
-            
-            <label htmlFor="email">Email :</label>
-            <input type="email" class="" id="inputEmail" placeholder="admin@gmail.com" required/>
+      <label htmlFor="tel" className='form-label'>Tel :</label>
+      <div className="col-sm-10">
+      <input type="text" class="form-control" id="inputTel" placeholder="055xxxxxxx" required/>
+      </div>
 
-            <label htmlFor="DeviceType"> Device Type :</label>
-            <select name="deviceType" id="inputDevicetype">
-                <option value="mobile">Mobile</option>
-                <option value="notebook">Notebook</option>
-                <option value="tablet">Tablet</option>
-                <option value="ipad">Ipad</option>
-                <option value="etc.">etc. please fill</option>
-            </select>
+      <label htmlFor="email" className='form-label'>Email :</label>
+      <div className="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail" placeholder="admin@gmail.com" required/>
+      </div>
 
-            <label htmlFor="deviceBrand">Device Brand :</label>
-            <input type="text" class="" id="inputdeviceBrand" placeholder="Apple , Sumsung , ..." required/>
+      <label htmlFor="DeviceType" className='form-label'> Device Type :</label>
+      <div className="col-sm-10">
+      <select name="deviceType" className='form-select' id="inputDevicetype">
+          <option value="mobile">Mobile</option>
+          <option value="notebook">Notebook</option>
+          <option value="tablet">Tablet</option>
+          <option value="ipad">Ipad</option>
+          <option value="etc.">etc. please fill</option>
+      </select>
+      </div>
 
-            <label htmlFor="deviceName">Device Name :</label>
-            <input type="text" class="" id="inputdeviceName" placeholder="" required/>
-        
-            <label htmlFor="startDate">Start Date :</label>
-            <input type="date" name="startDate" id="startDate" required/>
+      <label htmlFor="deviceBrand" className='form-label'>Device Brand :</label>
+      <div className="col-sm-10">
+      <input type="text" class="form-control" id="inputdeviceBrand" placeholder="Apple , Sumsung , ..." required/>
+      </div>
 
-            <label htmlFor="endDate">End Date :</label>
-            <input type="date" name="endDate" id="endDate" required/>
+      <label htmlFor="deviceName" className='form-label'>Device Name :</label>
+      <div className="col-sm-10">
+      <input type="text" class="form-control" id="inputdeviceName" placeholder="" required/>
+      </div>
 
-            <label htmlFor="remark">Remark :</label>
-            <input type="text" name="remark" id="remark" required/>
+      <label htmlFor="startDate" className='form-label'>Start Date :</label>
+      <div className="col-sm-10">
+      <input type="date" class="form-control" name="startDate" id="startDate" required/>
+      </div>
 
-            <input type="submit" value="Submit" />
-            
+      <label htmlFor="endDate" className='form-label'>End Date :</label>
+      <div className="col-sm-10">
+      <input type="date" class="form-control" name="endDate" id="endDate" required/>
+      </div>
 
-        </form>
+      <label htmlFor="remark" className='form-label'>Remark :</label>
+      <div className="col-sm-10">
+      <input type="text" class="form-control" name="remark" id="remark" required/>
+      </div>
+
+      <input type="submit" className='btn btn-primary' value="Submit" />
+
+
+      </form>
+      </div>
+      
       
     </div>
   );
