5b4a0f5b27edfc5f8121980067dd39b272caff1a
Gong <lovelymod6544@gmail.com>
2022-11-08 18:05:28 +0700

    add db

--- server/index.js
+++ server/index.js
@@ -12,3 +12,33 @@ const db = mysql.createConnection({
     password:"",
     database: "wifireq"
 })
+
+app.get('/wifirequest', (req,res) => {
+    db.query('SELECT * FROM wifirequest' , (err, result) => {
+        if(err){
+            console.log(err);
+        } else{
+            res.send(result);
+        }
+    })
+})
+
+app.post('/create', (req,res) => {
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const utype = req.body.utype;
+    const tel = req.body.tel;
+    const email = req.body.email;
+    const dtype = req.body.dtype;
+    const etc = req.body.etc;
+    const dbrand = req.body.dbrand;
+    const startdate = req.body.startdate;
+    const enddate = req.body.enddate;
+    const remark = req.body.remark;
+
+    db.query("INSERT INTO wifirequest (firstname, lastname, country, lastname, wage) VALUES (?,?,?,?,?)",)
+})
+
+app.listen('3001', () => {
+    console.log('Server running on port 3001');
+})
--- src/App.js
+++ src/App.js
@@ -1,12 +1,74 @@
 import './App.css';
 import {useState} from 'react'
+import Axios from 'axios';
+import axios from 'axios';
 
 
 function App() {
 
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+  const [utype, setUtype] = useState('');
+  const [tel, setTel] = useState('');
+  const [email, setEmail] = useState('');
+  const [dtype, setDtype] = useState('');
+  const [etc, setEtc] = useState('');
+  const [dbrand, setDbrand] = useState('');
+  const [dname, setDname] = useState('');
+  const [startdate, setStartdate] = useState('');
+  const [enddate, setEnddate] = useState('');
+  const [remark, setRemark] = useState('');
+
+  const [wifireqList ,setWifiReqList] = useState([]);
+
+  const getRequest = () => {
+    Axios.get('http://localhost:3001/wifirequest').then((Response) => {
+      setWifiReqList(Response.data);
+    })
+  }
+
+  const addRequest = () =>{
+    Axios.post('http://localhost:3001/create',{
+      firstname: fname,
+      lastname: lname,
+      usertype: utype,
+      tel: tel,
+      email: email,
+      devicetype: dtype,
+      devicetype: etc,
+      devicebrand: dbrand,
+      devicename: dname,
+      startdate: startdate,
+      enddate: enddate,
+      remark: remark,
+    }).then(() => {
+      setWifiReqList([
+        ...wifireqList,{
+          firstname: fname,
+        lastname: lname,
+        usertype: utype,
+        tel: tel,
+        email: email,
+        devicetype: dtype,
+        devicetype: etc,
+        devicebrand: dbrand,
+        devicename: dname,
+        startdate: startdate,
+        enddate: enddate,
+        remark: remark,
+        }
+      ])
+    })
+  }
+
+  
+      
+
   
   const [IsDisable,setIsDisable] = useState('true');
   const [etcDisable,setetcDisable] = useState('true');
+
+
 
   const Check = (value) => {
       if(value=="staff"){
@@ -40,17 +102,17 @@ function App() {
 
       <label htmlFor="inputFname" className='form-label'>Firstname :</label>
       <div className="col-sm-10">
-      <input type="text" className="form-control" id="inputFirstname" placeholder="Firstname(English)" />
+      <input type="text" className="form-control" id="inputFirstname" placeholder="Firstname(English)" onChange={(e) => {setFname(e.target.value)}}/>
       </div>
 
       <label htmlFor="inputLname" className='form-label'>Lastname :</label>
       <div className="col-sm-10">
-      <input type="text" className="form-control" id="inputLastname" placeholder="Lastname(English)" />
+      <input type="text" className="form-control" id="inputLastname" placeholder="Lastname(English)" onChange={(e) => {setLname(e.target.value)}}/>
       </div>
 
       <label htmlFor="UserType" className='form-label'> User Type :</label>
       <div className="col-sm-10">
-      <select name="userType" className='form-select' id="inputUsertype" onChange={(e) => Check(e.target.value)}>
+      <select name="userType" className='form-select' id="inputUsertype" onChange={(e) => {Check(e.target.value); setUtype(e.target.value)}}>
           <option value="staff" >Staff</option>
           <option value="internship">Internship</option>
           <option value="guest">Guest</option>
@@ -59,17 +121,17 @@ function App() {
 
       <label htmlFor="tel" className='form-label'>Tel :</label>
       <div className="col-sm-10">
-      <input type="text" className="form-control" id="inputTel" placeholder="055xxxxxxx" />
+      <input type="text" className="form-control" id="inputTel" placeholder="055xxxxxxx" onChange={(e) => {setTel(e.target.value)}}/>
       </div>
 
       <label htmlFor="email" className='form-label'>Email :</label>
       <div className="col-sm-10">
-      <input type="email" className="form-control" id="inputEmail" placeholder="admin@gmail.com" />
+      <input type="email" className="form-control" id="inputEmail" placeholder="admin@gmail.com" onChange={(e) => {setEmail(e.target.value)}}/>
       </div>
 
       <label htmlFor="DeviceType" className='form-label'> Device Type :</label>
       <div className="col-sm-10">
-      <select name="deviceType" className='form-select' id="inputDevicetype" onChange={(e) => Checketc(e.target.value)}>
+      <select name="deviceType" className='form-select' id="inputDevicetype" onChange={(e) => {Checketc(e.target.value);setDtype(e.target.value)}}>
           <option value="mobile">Mobile</option>
           <option value="notebook">Notebook</option>
           <option value="tablet">Tablet</option>
@@ -79,32 +141,32 @@ function App() {
       </div>
 
       <div className="col-sm-10">
-      <input disabled={etcDisable} type="text" className="form-control etc" id="inputEtc" placeholder="Etc please fill ..." />
+      <input disabled={etcDisable} type="text" className="form-control etc" id="inputEtc" placeholder="Etc please fill ..." onChange={(e) => {setEtc(e.target.value)}}/>
       </div>
 
       <label htmlFor="deviceBrand" className='form-label'>Device Brand :</label>
       <div className="col-sm-10">
-      <input type="text" className="form-control" id="inputdeviceBrand" placeholder="Apple , Sumsung , ..." />
+      <input type="text" className="form-control" id="inputdeviceBrand" placeholder="Apple , Sumsung , ..." onChange={(e) => {setDbrand(e.target.value)}}/>
       </div>
 
       <label htmlFor="deviceName" className='form-label'>Device Name :</label>
       <div className="col-sm-10">
-      <input type="text" className="form-control" id="inputdeviceName" placeholder="" />
+      <input type="text" className="form-control" id="inputdeviceName" placeholder="" onChange={(e) => {setDname(e.target.value)}}/>
       </div>
 
       <label htmlFor="startDate" className='form-label'>Start Date :</label>
       <div className="col-sm-10">
-      <input type="date" className="form-control" name="startDate" id="startDate" />
+      <input type="date" className="form-control" name="startDate" id="startDate" onChange={(e) => {setStartdate(e.target.value)}}/>
       </div>
 
       <label htmlFor="endDate" className='form-label'>End Date :</label>
       <div className="col-sm-10">
-      <input disabled={IsDisable} type="date" className="form-control" name="endDate" id="endDate" />
+      <input disabled={IsDisable} type="date" className="form-control" name="endDate" id="endDate" onChange={(e) => {setEnddate(e.target.value)}}/>
       </div>
 
       <label htmlFor="remark" className='form-label'>Remark :</label>
       <div className="col-sm-10">
-      <input type="text" className="form-control" name="remark" id="remark" />
+      <input type="text" className="form-control" name="remark" id="remark" onChange={(e) => {setRemark(e.target.value)}}/>
       </div>
 
       <input  type="" className='btn btn-primary' value="Submit" />
