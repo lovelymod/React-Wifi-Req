16c187752cc541c4942c453b00969642d908dac4
Gong <lovelymod6544@gmail.com>
2022-11-09 13:49:03 +0700

    add new

--- server/index.js
+++ server/index.js
@@ -7,14 +7,14 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    user: "root",
+    user: "gong",
     host: "localhost",
-    password:"",
-    database: "wifireq"
+    password:"gong1234",
+    database: "wifireq2"
 })
 
 app.get('/wifirequest', (req,res) => {
-    db.query('SELECT * FROM wifirequest' , (err, result) => {
+    db.query('SELECT * FROM wifireq2' , (err, result) => {
         if(err){
             console.log(err);
         } else{
@@ -23,20 +23,33 @@ app.get('/wifirequest', (req,res) => {
     })
 })
 
+
+
 app.post('/create', (req,res) => {
-    const fname = req.body.fname;
-    const lname = req.body.lname;
-    const utype = req.body.utype;
+    const fname = req.body.firstname
+    const lname = req.body.lastname;
+    const utype = req.body.usertype;
     const tel = req.body.tel;
     const email = req.body.email;
     const dtype = req.body.dtype;
-    const etc = req.body.etc;
+    // const etc = req.body.etc;
     const dbrand = req.body.dbrand;
+    const dname = req.body.dname;
     const startdate = req.body.startdate;
     const enddate = req.body.enddate;
     const remark = req.body.remark;
 
-    db.query("INSERT INTO wifirequest (firstname, lastname, country, lastname, wage) VALUES (?,?,?,?,?)",)
+    db.query("INSERT INTO wifireq2 (firstname, lastname, usertype, tel, email, dtype, dbrand, dname, startdate, enddate, remark) VALUES (?,?,?,?,?,?,?,?,?,?,?)", 
+    [fname,lname,utype,tel,email,dtype,dbrand,dname,startdate,enddate,remark],
+    (err,result) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.send('Value Inserted');
+        }
+    }
+    );
+
 })
 
 app.listen('3001', () => {
