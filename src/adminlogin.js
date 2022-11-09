4aebaf24ba9412db8a8592f61baf9fdc57374c1b
Gong <lovelymod6544@gmail.com>
2022-11-09 17:02:13 +0700

    add new

--- src/adminlogin.js
+++ src/adminlogin.js
@@ -1,14 +1,43 @@
-import "./login.css"
+import "./login.css";
 
-const Login = () => {
-    return ( 
-        <div className="container">
-            <div className="bg">
-            
-            </div>
+function Login() {
+  return (
+    <div className="App2">
+      <div className="bg2">
+        <div className="images">
+          <img className="logo" src="img/LS-01.png" alt="" srcSet="" />
         </div>
+        <div className="container">
+          <form>
+            <div className="row-contain">
+              <label htmlFor="Username" className="form-label">
+                Username :
+              </label>
 
-     );
+              <input
+                type="text"
+                className="form-control"
+                id=""
+                placeholder=""
+              />
+            </div>
+            <div className="row-contain">
+              <label htmlFor="Password" className="form-label">
+                Password :
+              </label>
+
+              <input
+                type="text"
+                className="form-control"
+                id=""
+                placeholder=""
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
- 
+
 export default Login;
