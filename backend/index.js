6f7059753a1af754c857fc59950a2acb0373e641
Gong <lovelymod6544@gmail.com>
2023-02-02 17:27:25 +0700

    not thing

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(UserRoute);
